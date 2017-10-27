472724817e77576ca8dda353e33047674896813b
Linus <ljschumacher@users.noreply.github.com>
2017-10-27 10:52:01 +0100

    fixed bug in calculateSpeedsFromSkeleton.m spotted by Serena

--- calculateSpeedsFromSkeleton.m
+++ calculateSpeedsFromSkeleton.m
@@ -27,19 +27,19 @@ velocity_y = dydFrame./dFramedt;
 [~, dyds] = gradient(squeeze(skelData(2,skelIndcs,:)),-1);
 [~, dxds] = gradient(squeeze(skelData(1,skelIndcs,:)),-1);
 % sign speed based on relative orientation of velocity to body
-speedSigned = getSignedSpeed(velocity,[mean(dxds); mean(dyds)]);
+speedSigned = getSignedSpeed([velocity_x; velocity_y],[mean(dxds); mean(dyds)]);
 if filter
-% ignore first and last frames of each worm's track
-wormChangeIndcs = gradient(double(trajData.worm_index_joined))~=0;
-speedSigned(wormChangeIndcs)=NaN;
-% ignore frames with bad skeletonization
-speedSigned(trajData.is_good_skel~=1)=NaN;
-% ignore skeletons otherwise filtered out
-speedSigned(~trajData.filtered) = NaN;
+    % ignore first and last frames of each worm's track
+    wormChangeIndcs = gradient(double(trajData.worm_index_joined))~=0;
+    speedSigned(wormChangeIndcs)=NaN;
+    % ignore frames with bad skeletonization
+    speedSigned(trajData.is_good_skel~=1)=NaN;
+    % ignore skeletons otherwise filtered out
+    speedSigned(~trajData.filtered) = NaN;
 end
 if smoothingWindowSize>0
-% smooth speed to denoise
-speedSigned = smoothdata(speedSigned,'movmean',smoothingWindowSize,'omitnan');
+    % smooth speed to denoise
+    speedSigned = smoothdata(speedSigned,'movmean',smoothingWindowSize,'omitnan');
 end
 
 end
