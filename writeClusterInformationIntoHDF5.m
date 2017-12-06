2bc5a65aca7b7754bb56574bebf3f5510babf109
ljschumacher <lschum@me.com>
2017-12-06 09:47:28 +0000

    calculating distances to 10 nearest nbrs for each skeleton part

--- writeClusterInformationIntoHDF5.m
+++ writeClusterInformationIntoHDF5.m
@@ -45,6 +45,20 @@ for strainCtr = 1:length(strains)
                 filterSkelLength(skelData_r,pixelsize,minSkelLength,maxSkelLength);
             %% calculate stats - red channel files
             try
+                neighbr_distances_skeleton = h5read(filename_r,'/neighbr_distances_skeleton');
+            catch
+                disp(['Calculating skeleton distances from ' filename_r ])
+                neighbr_distances_skeleton ...
+                    = calculateNeighbrDistanceFromSkeleton(skelData_r,trajData_r,trajData_g,pixelsize);
+                assert(size(neighbr_distances_skeleton,1)==length(trajData_r.frame_number))
+                assert(size(neighbr_distances_skeleton,2)==size(skelData_r,2));
+                assert(size(neighbr_distances_skeleton,3)==10)
+                % write stats to hdf5-file
+                h5create(filename_r,'/neighbr_distances_skeleton',size(neighbr_distances_skeleton),...
+                    'Datatype','single')
+                h5write(filename_r,'/neighbr_distances_skeleton',single(neighbr_distances_skeleton))
+            end
+            try
                 neighbr_distances = h5read(filename_r,'/neighbr_distances');
                 min_neighbr_dist = h5read(filename_r,'/min_neighbr_dist');
                 num_close_neighbrs = h5read(filename_r,'/num_close_neighbrs');
@@ -58,17 +72,29 @@ for strainCtr = 1:length(strains)
                 assert(length(min_neighbr_dist)==length(trajData_r.frame_number))
                 assert(length(num_close_neighbrs)==length(trajData_r.frame_number))
                 % write stats to hdf5-file
-                h5create(filename_r,'/neighbr_distances',size(neighbr_distances),...
-                    'Datatype','single')
-                h5write(filename_r,'/neighbr_distances',single(neighbr_distances))
-                h5create(filename_r,'/min_neighbr_dist',...
-                    size(min_neighbr_dist),'Datatype','single')
-                h5write(filename_r,'/min_neighbr_dist',...
-                    single(min_neighbr_dist))
-                h5create(filename_r,'/num_close_neighbrs',...
-                    size(num_close_neighbrs),'Datatype','uint16')
-                h5write(filename_r,'/num_close_neighbrs',...
-                    uint16(num_close_neighbrs))
+                try
+                    h5create(filename_r,'/neighbr_distances',size(neighbr_distances),...
+                        'Datatype','single')
+                    h5write(filename_r,'/neighbr_distances',single(neighbr_distances))
+                catch
+                    disp('/neighbr_distances already exists')
+                end
+                try
+                    h5create(filename_r,'/min_neighbr_dist',...
+                        size(min_neighbr_dist),'Datatype','single')
+                    h5write(filename_r,'/min_neighbr_dist',...
+                        single(min_neighbr_dist))
+                catch
+                    disp('/min_neighbr_dist already exists')
+                end
+                try
+                    h5create(filename_r,'/num_close_neighbrs',...
+                        size(num_close_neighbrs),'Datatype','uint16')
+                    h5write(filename_r,'/num_close_neighbrs',...
+                        uint16(num_close_neighbrs))
+                catch
+                    disp('/num_close_neighbrs already exists')
+                end
             end
             % calculate stats - green channel files
             try
