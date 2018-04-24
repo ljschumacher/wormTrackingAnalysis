80f045ac35eeaa8aca7fcbe3f44a0d4a9fa2512c
Serena Ding <sding1@ld-rendres11.bc.ic.ac.uk>
2018-04-24 15:45:13 +0100

    commit to test video making on a different screen

--- visualiseSweeping.m
+++ visualiseSweeping.m
@@ -8,10 +8,19 @@ close all
 clear
 
 dataset = 2;
-phase = 'sweeping';
+phase = 'fullMovie';
 wormnum = '40';
 markerType = 'pharynx';
-plotDiagnostics = true;
+numMovieSlices = 10;
+
+if numMovieSlices>1
+    % frame slice: n slices of 1 minutes (9 fps * 60s)
+    frameSlices = zeros(numMovieSlices,2);
+    for sliceCtr = 1:numMovieSlices
+        frameSlices(sliceCtr,:) = [round(32400/numMovieSlices*(sliceCtr-1)) round(32400/numMovieSlices*(sliceCtr-1)+540)];
+    end
+end
+
 
 addpath('auxiliary/')
 addpath('visualisation/')
@@ -24,8 +33,6 @@ if dataset ==1
 elseif dataset ==2
     strains = {'npr1','N2'};
 end
-useJoinedTraj = true;
-
 nStrains = length(strains);
 
 % filtering parameters
@@ -37,21 +44,13 @@ end
 if strcmp(markerType,'pharynx')
     maxBlobSize = 1e5; 
     channelStr = 'g';
-elseif strcmp(markerType,'bodywall')
-    maxBlobSize = 2.5e5;
-    channelStr = 'r';
-    minSkelLength = 850;
-    maxSkelLength = 1500;
 else
     error('unknown marker type specified, should be pharynx or bodywall')
 end
 % analysis parameters
 pixelsize = 100/19.5; % 100 microns are 19.5 pixels
-% plotting parameters
-plotColors = lines(nStrains);
-if plotDiagnostics, visitfreqFig = figure; hold on, end
 % export fig parameters
-exportOptions = struct('Format','eps2',...
+exportOptions = struct('Format','EPS2',...
     'Color','rgb',...
     'Width',10,...
     'Resolution',300,...
@@ -60,35 +59,29 @@ exportOptions = struct('Format','eps2',...
     'LineWidth',1);
 
 %% loop through strains
-for strainCtr = 1:nStrains
+for strainCtr = 1%:nStrains
     %% load file lists
     if dataset == 1
         [phaseFrames,filenames,~] = xlsread(['datalists/' strains{strainCtr} '_' wormnum '_list.xlsx'],1,'A1:E15','basic');
     elseif dataset == 2
         [phaseFrames,filenames,~] = xlsread(['datalists/' strains{strainCtr} '_' wormnum '_' channelStr '_list.xlsx'],1,'A1:E15','basic');
     end
-    if ~useJoinedTraj
-        filenames = strrep(filenames,'/data2/shared/data/twoColour/Results/',...
-            '/end/home/lschumac/databackup/data/twoColour/ResultsUnjoinedTrajectories/');
-    end
     numFiles = length(filenames);
     if strcmp(wormnum,'40'), visitfreq = cell(numFiles,1); end
     %% loop through files
-    for fileCtr = 1:numFiles % can be parfor
+    for fileCtr = 1%:numFiles % can be parfor
         filename = filenames{fileCtr};
+        %% make new video
+        writerObj = VideoWriter(['figures/individualRecordings/' strains{strainCtr} '_' strrep(strrep(filename(end-32:end-18),' ',''),'/','') '_sitesVisited.avi']);
+        writerObj.FrameRate = 2;
+        open(writerObj);
         %% load tracking data
         trajData = h5read(filename,'/trajectories_data');
         blobFeats = h5read(filename,'/blob_features');
-        %% randomly sample which frames to analyze
+        %% sample which frames to analyze
         frameRate = double(h5readatt(filename,'/plate_worms','expected_fps'));
         [firstFrame, lastFrame] = getPhaseRestrictionFrames(phaseFrames,phase,fileCtr);
-        numFrames = round((lastFrame-firstFrame)/frameRate/3);
-        framesAnalyzed = randperm((lastFrame-firstFrame),numFrames) + firstFrame; % randomly sample frames without replacement
         %% filter data for worms
-        if plotDiagnostics
-            visualizeIntensitySizeFilter(blobFeats,pixelsize,intensityThresholds(wormnum),maxBlobSize,...
-                [wormnum ' ' strains{strainCtr} ' ' strrep(filename(end-32:end-18),'/','')])
-        end
         if strcmp(markerType,'pharynx')
             % reset skeleton flag for pharynx data
             trajData.has_skeleton = true(size(trajData.has_skeleton)); %squeeze(~any(any(isnan(skelData))));
@@ -105,39 +98,39 @@ for strainCtr = 1:nStrains
         % apply phase restriction
         phaseFilter_logInd = trajData.frame_number < lastFrame & trajData.frame_number > firstFrame;
         trajData.filtered(~phaseFilter_logInd)=false;
-
-        %% heat map of sites visited - this only makes sense for 40 worm dataset where we don't move the camera
-        if strcmp(wormnum,'40')&& plotDiagnostics
-            siteVisitFig = figure;
-            x = trajData.coord_x(trajData.filtered);
-            y = trajData.coord_y(trajData.filtered);
-            h=histogram2(x*pixelsize/1000,y*pixelsize/1000,...
-                'DisplayStyle','tile','EdgeColor','none','Normalization','pdf');
-            visitfreq{fileCtr} = h.Values(:);
-            cb = colorbar; cb.Label.String = '# visited';
-            axis equal
-            xlabel('x (mm)'), ylabel('y (mm)')
-            title([strains{strainCtr} ' ' strrep(filename(end-32:end-18),'/','')])
-            set(siteVisitFig,'PaperUnits','centimeters')
-            figurename = ['figures/individualRecordings/' strains{strainCtr}...
-                '_' strrep(strrep(filename(end-32:end-18),' ',''),'/','') '_sitesVisited' '_' phase '_data' num2str(dataset)];
-            %exportfig(siteVisitFig,[figurename '.eps'],exportOptions)
+        % select frame slices
+        for sliceCtr = 1:size(frameSlices,1)
+            sliceStart = frameSlices(sliceCtr,1);
+            sliceEnd = frameSlices(sliceCtr,2);
+            sliceLogInd = false(size(trajData.filtered));
+            for frameCtr = sliceStart:sliceEnd
+                sliceLogInd(frameCtr==trajData.frame_number)=true;
+            end
+            %% heat map of sites visited - this only makes sense for 40 worm dataset where we don't move the camera
+            if strcmp(wormnum,'40')
+                siteVisitFig = figure;
+                x = trajData.coord_x(trajData.filtered & sliceLogInd);
+                y = trajData.coord_y(trajData.filtered & sliceLogInd);
+                h=histogram2(x*pixelsize/1000,y*pixelsize/1000,...
+                    'DisplayStyle','tile','EdgeColor','none','Normalization','count');
+                visitfreq{fileCtr} = h.Values(:);
+                cb = colorbar; cb.Label.String = '# visited';
+                caxis([0 1500]);
+                xlim([0 12]);
+                ylim([0 12]);
+                xlabel('x (mm)'), ylabel('y (mm)')
+                title([strains{strainCtr} ' ' strrep(filename(end-32:end-18),'/','') ', ' num2str(round((sliceCtr-1)*60/numMovieSlices)) 'min'])
+                set(siteVisitFig,'PaperUnits','centimeters')
+                figurename = ['figures/individualRecordings/' strains{strainCtr}...
+                    '_' strrep(strrep(filename(end-32:end-18),' ',''),'/','') '_sitesVisited' '_' phase '_slice' num2str(sliceCtr) '_data' num2str(dataset)];
+                saveas(gcf,[figurename '.tif'])
+                % write heatmap to video
+                image = imread([figurename '.tif']);
+                frame = im2frame(image);
+                writeVideo(writerObj,frame)
+                system(['rm ' figurename '.tif']);
+            end
         end
+        close(writerObj);
     end
-     %% combine data from multiple files
-         % plot frequency of visited sites
-    if  strcmp(wormnum,'40')&& plotDiagnostics
-        histogram(visitfreqFig.Children,vertcat(visitfreq{:}),'DisplayStyle','stairs','Normalization','pdf')
-    end
-end
-% heatmap of sites visited
-if  strcmp(wormnum,'40')&& plotDiagnostics
-    visitfreqFig.Children.XScale = 'log';
-    visitfreqFig.Children.YScale = 'log';
-    %         visitfreqFig.Children.XLim = [4e-5 1e-1];
-    xlabel(visitfreqFig.Children,'site visit frequency, f')
-    ylabel(visitfreqFig.Children,'pdf p(f)')
-    legend(visitfreqFig.Children,strains)
-    figurename = ['figures/visitfreq_' wormnum '_' markerType];
-    exportfig(visitfreqFig,[figurename '.eps'],exportOptions)
 end
