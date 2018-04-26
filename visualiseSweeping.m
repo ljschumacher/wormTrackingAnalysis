e27e4eb3ddf33d45d5ee2c87c0181753c5f3a60a
Serena Ding <ding.serena@gmail.com>
2018-04-26 16:35:01 +0100

    finalise sweeping visualisation script and add all relevant plots and movies

--- visualiseSweeping.m
+++ visualiseSweeping.m
@@ -1,6 +1,8 @@
-% dataset: 1 or 2. To specify which dataset to run the script for.
-% phase: 'joining', 'fullMovie', or 'sweeping'. Script defines stationary phase as: starts at 10% into the movie, and stops at 60% into the movie (HA and N2) or at specified stopping frames (npr-1).
-% markerType: 'pharynx'
+% script visualises sweeping in 40 worm pharynx datasets in several ways.
+% 1. phase-specific histogram of sites visited based on trajData (plot): makeVideo = false; useBlobThreshold = false;
+% 2. time-lapse histogram of sites visited based on trajData (movie): makeVideo = true; useBlobThreshold = false;
+% 3. time-lapse, binary intensity thresholding of site visit histograms based on trajData (movie): makeVideo = true; useBlobThreshold = true;
+% 4. intensity and area thresholding of site visit histograms based on trajData (plot): makeVideo = true; useBlobThreshold = true; plotClusters = true;
 
 close all
 clear
@@ -11,12 +13,12 @@ phase = 'fullMovie';
 wormnum = '40';
 markerType = 'pharynx';
 numMovieSlices = 12;
-useBlobThreshold = true;
+useBlobIntensityThreshold = true;
 makeVideo = false;
 
-if useBlobThreshold
-    blobHeatMapIntensityThreshold = 200;
-    blobAreaThreshold = 500;
+if useBlobIntensityThreshold
+    blobHeatMapIntensityThreshold = 200; %255 is white, 0 is black on grayscale
+    blobAreaThreshold = 750;
     plotClusters = true;
 end
 
@@ -28,7 +30,6 @@ if dataset ==1
 elseif dataset ==2
     strains = {'npr1','N2'};
 end
-nStrains = length(strains);
 pixelsize = 100/19.5; % 100 microns are 19.5 pixels
 
 % filtering parameters
@@ -69,12 +70,12 @@ if numMovieSlices>1
     end
 end
 
-if useBlobThreshold
+if useBlobIntensityThreshold
     plotColors = parula(numMovieSlices);
 end
 
 %% loop through strains
-for strainCtr = 2%1:nStrains
+for strainCtr = 1:length(strains)
     %% load file lists
     if dataset == 1
         [phaseFrames,filenames,~] = xlsread(['datalists/' strains{strainCtr} '_' wormnum '_list.xlsx'],1,'A1:E15','basic');
@@ -82,13 +83,12 @@ for strainCtr = 2%1:nStrains
         [phaseFrames,filenames,~] = xlsread(['datalists/' strains{strainCtr} '_' wormnum '_' channelStr '_list.xlsx'],1,'A1:E15','basic');
     end
     numFiles = length(filenames);
-    if strcmp(wormnum,'40'), visitfreq = cell(numFiles,1); end
     %% loop through files
     for fileCtr = 1:numFiles % can be parfor
         filename = filenames{fileCtr};
         %% make new video
         if makeVideo
-            if useBlobThreshold
+            if useBlobIntensityThreshold
                 writerObj = VideoWriter(['figures/sweeping/' strains{strainCtr} '_' strrep(strrep(filename(end-32:end-18),' ',''),'/','') '_sitesVisited_blobs.avi']);
             else
                 writerObj = VideoWriter(['figures/sweeping/' strains{strainCtr} '_' strrep(strrep(filename(end-32:end-18),' ',''),'/','') '_sitesVisited.avi']);
@@ -109,8 +109,7 @@ for strainCtr = 2%1:nStrains
         end
         trajData.filtered = filterIntensityAndSize(blobFeats,pixelsize,...
             intensityThresholds(wormnum),maxBlobSize)...
-            &trajData.has_skeleton; % careful: we may not want to filter for skeletonization for
-            %clustering statistics
+            &trajData.has_skeleton; % careful: we may not want to filter for skeletonization for clustering statistics
         if strcmp(markerType,'bodywall')
             % filter red data by skeleton length
             trajData.filtered = trajData.filtered&logical(trajData.is_good_skel)&...
@@ -122,6 +121,7 @@ for strainCtr = 2%1:nStrains
         % create figure to hold cluster outline plots
         if plotClusters
             clusterOutlineFig = figure; hold on
+            blobAreas.(strains{strainCtr}){fileCtr} = NaN(numMovieSlices,10);
         end
         % select frame slices
         for sliceCtr = 1:size(frameSlices,1)
@@ -144,12 +144,11 @@ for strainCtr = 2%1:nStrains
                 xlabel('x (mm)'), ylabel('y (mm)')
                 xlim([0 12]);
                 ylim([0 12]);
-                visitfreq{fileCtr} = h.Values(:);
                 set(siteVisitFig,'PaperUnits','centimeters')
                 figurename = ['figures/sweeping/' strains{strainCtr}...
                     '_' strrep(strrep(filename(end-32:end-18),' ',''),'/','') '_sitesVisited' '_' phase '_slice' num2str(round((sliceCtr-1)*60/numMovieSlices)) '_data' num2str(dataset)];
 
-                if useBlobThreshold
+                if useBlobIntensityThreshold
                     axis off
                     colorbar off
                     saveas(gcf,[figurename '.tif']) % save black and white heat map image without labels for blob thresholding
@@ -157,34 +156,38 @@ for strainCtr = 2%1:nStrains
                     image = rgb2gray(image);
                     binaryImage = image < blobHeatMapIntensityThreshold; % apply blob heat map intensity threshold values
                     binaryImage = imfill(binaryImage, 'holes');
-                    % need to give binary image a handle %%%%%%%%%%%%%
+                    imshow(binaryImage)
                     set(gcf,'PaperUnits','centimeters')
                     %xlim([0 12]);
                     %ylim([0 12]);
                     if plotClusters
                         set(0,'CurrentFigure',clusterOutlineFig)
-                        labeledImage = bwlabel(binaryImage, 8);     % Label each blob so we can make measurements of it
+                        labeledImage = bwlabel(binaryImage, 8); % label each blob so we can make measurements of it
                         blobMeasurements = regionprops(binaryImage, 'Area');
+                        if numel([blobMeasurements.Area])>0
+                            blobAreas.(strains{strainCtr}){fileCtr}(sliceCtr,1:numel([blobMeasurements.Area])) = [blobMeasurements.Area];
+                        end
                         blobLogInd = [blobMeasurements.Area] > blobAreaThreshold; % apply blob area threshold values
                         blobBoundaries = bwboundaries(binaryImage);
                         for blobCtr = 1:numel(blobLogInd) % plot individual blob boundaries that meet area threshold requirements
                             if blobLogInd(blobCtr)
-                                plot(blobBoundaries{blobCtr}(:,1),blobBoundaries{blobCtr}(:,2),'Color', plotColors(sliceCtr,:),'LineWidth',0.5)
+                                plot(blobBoundaries{blobCtr}(:,2)/size(binaryImage,2)*12,...
+                                    blobBoundaries{blobCtr}(:,1)/size(binaryImage,1)*12,...
+                                    'Color', plotColors(sliceCtr,:),'LineWidth',0.5) % reset the size of the plot to 12x12 cm
                             end
                         end
-                    else %%%%%%% need to fix this part of code instead of else give that figure a handle and run it regardless
+                    else 
                         cb = colorbar; cb.Label.String = '# visited';
-                        xlabel('x (mm)'), ylabel('y (mm)')
+                        xlabel('x (pixels)'), ylabel('y (pixels)') % binary images are 1167x875 pixels
                         title([strains{strainCtr} ' ' strrep(filename(end-32:end-18),'/','') ', ' num2str(round((sliceCtr-1)*60/numMovieSlices)) 'min'])
                         axis on
-                        %set(gca,'Ydir','Normal')
                         colorbar off
                         colormap(colorMap)
                         saveas(gcf,[figurename '.tif'])
                     end
                 end
                 
-                if ~useBlobThreshold
+                if ~useBlobIntensityThreshold
                     title([strains{strainCtr} ' ' strrep(filename(end-32:end-18),'/','') ', ' num2str(round((sliceCtr-1)*60/numMovieSlices)) 'min'])
                     saveas(gcf,[figurename '.tif'])
                 end
@@ -201,10 +204,16 @@ for strainCtr = 2%1:nStrains
         % format and export cluster plot from this recording
         if plotClusters
             set(0,'CurrentFigure',clusterOutlineFig)
-            xlim([0 size(binaryImage,1)])
-            ylim([0 size(binaryImage,2)])
+            set(gca,'Ydir','reverse')
+            xlim([0 12])
+            ylim([0 12])
+            xticks([0:2:12])
+            yticks([0:2:12])
             xlabel('x (mm)'), ylabel('y (mm)')
             title([strains{strainCtr} ' ' strrep(filename(end-32:end-18),'/','')])
+            colorbar
+            caxis([0 60])
+            cb = colorbar; cb.Label.String = 'minutes'; 
             figurename = ['figures/sweeping/' strains{strainCtr}...
                 '_' strrep(strrep(filename(end-32:end-18),' ',''),'/','') '_blobsOverTime_' phase '_data' num2str(dataset)];
             exportfig(clusterOutlineFig,[figurename '.eps'],exportOptions)
